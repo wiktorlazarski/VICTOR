0a83ad6a8a2bf12bacafda80a215d1006d73a869
Wiktor Lazarski <wjlazarski@gmail.com>
2021-02-10 23:34:57 +0100

    Sending POST request with image to be caption

--- app/src/main/java/com/example/victor/ProphecyActivity.kt
+++ app/src/main/java/com/example/victor/ProphecyActivity.kt
@@ -1,26 +1,40 @@
 package com.example.victor
 
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
+import android.util.Base64
+import android.util.Log
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
+import java.io.ByteArrayOutputStream
 import java.io.File
 
+
 class ProphecyActivity : AppCompatActivity() {
+    private var requestQueue: RequestQueue? = null
     private var currPhotoPath: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_prophecy)
 
+        requestQueue = Volley.newRequestQueue(this)
+
         val fileName = "photo"
         val storageDirectory = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
 
-        val imageFile = File.createTempFile(fileName, ".jpg", storageDirectory)
+        val imageFile = File.createTempFile(fileName, ".jpeg", storageDirectory)
 
         currPhotoPath = imageFile.absolutePath
         val imageUri = FileProvider.getUriForFile(
@@ -32,11 +46,6 @@ class ProphecyActivity : AppCompatActivity() {
         val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
         cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri)
         startActivityForResult(cameraIntent, 1)
-        // Start camera intent
-
-        // Send image to server
-
-        // Receive caption and display
     }
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -48,6 +57,25 @@ class ProphecyActivity : AppCompatActivity() {
             findViewById<ImageView>(R.id.captured_image_view).apply {
                 setImageBitmap(bitmap)
             }
+
+            val byteStream = ByteArrayOutputStream()
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, byteStream)
+            val imageBytes = byteStream.toByteArray()
+            val imageString = Base64.encodeToString(imageBytes, Base64.DEFAULT)
+
+            val jsonBody = JSONObject()
+            jsonBody.put("image", imageString)
+
+            val request = JsonObjectRequest(
+                Request.Method.POST,
+                "http://192.168.8.101:1234/caption",
+                jsonBody,
+                { response -> findViewById<TextView>(R.id.prophecy_view).apply {
+                    text = response.get("caption").toString()
+                } },
+                { error -> Log.d("RESPONSE err", "error->$error") })
+
+            requestQueue?.add(request)
         }
         else {
             val startActivityIntent = Intent(this, MainActivity::class.java)
